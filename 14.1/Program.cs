0805512d9872541c8930ae8c6a3a824dae016192
Fargonchik <39383172+Fargonchik@users.noreply.github.com>
2018-06-26 20:42:58 +0300

    111
    11

---
+++ 14.1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14._1
+{
+    class Program
+    {
+        public class TimeStamp
+        {
+            protected int timeInSec;
+
+            public int TimeInHours {
+                get { return timeInSec / 3600; }
+                set { timeInSec = value * 3600; }
+            }
+        }
+        static void Main(string[] args)
+        {
+            var t = new TimeStamp();
+            t.TimeInHours = 2;
+            Console.WriteLine($"Time in Hours = {t.TimeInHours}");
+        }
+    }
+}
